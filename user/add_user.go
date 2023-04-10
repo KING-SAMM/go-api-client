aee6c1355864d37172c227f892a798fd0b40ce8e
kcsamm <kcsamm11@gmail.com>
2023-04-10 18:26:38 +0100

    Modified Add User functionality
    Included status/error message display

--- user/add_user.go
+++ user/add_user.go
@@ -9,41 +9,68 @@ import (
 	"encoding/json"
 )
 
+type Msg struct {
+	Success string
+	Error string
+}
+
 func AddUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+
+		var msg Msg
 
 		firstname := r.FormValue("firstname")
 		lastname := r.FormValue("lastname")
 		email := r.FormValue("email")
-		user := User{
-			Firstname: firstname,
-			Lastname: lastname,
-			Email: email,
-		}
 
-		userjson, err := json.Marshal(user)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("User is: %v\n", user)
-		// fmt.Printf("Userjson byte slice is: %v\n", userjson)
-		fmt.Printf("Userjson string is: %v\n", string(userjson))
+		// If any field is empty
+		if firstname != "" && lastname != "" && email != "" {
+			user := User{
+				Firstname: firstname,
+				Lastname: lastname,
+				Email: email,
+			}
+	
+			userjson, err := json.Marshal(user)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("User is: %v\n", user)
+			// fmt.Printf("Userjson byte slice is: %v\n", userjson)
+			fmt.Printf("Userjson string is: %v\n", string(userjson))
+	
+			resp, err := http.Post("http://localhost:3000/user", "application/json", bytes.NewBuffer(userjson))
+			if err != nil {
+				log.Fatal(err)
+			}
+	
+			defer resp.Body.Close()
+	
+			log.Printf("Response status: %s", resp.Status)
+	
+			if resp.Status == "200 OK" {
+				templ, _ := template.ParseFiles(
+					"template/add.html",
+					"template/err.html",
+				)
 
-		resp, err := http.Post("http://localhost:3000/user", "application/json", bytes.NewBuffer(userjson))
-		if err != nil {
-			log.Fatal(err)
-		}
+				msg.Success = "User created successfully"
+				msg.Error = ""
+		
+				templ.Execute(w, &msg)
+			}
+		} else {
+			templ, _ := template.ParseFiles(
+				"template/add.html",
+				"template/err.html",
+			)
 
-		defer resp.Body.Close()
+			msg.Error = "No empty fields allowed"
+			msg.Success = ""
+	
+			templ.Execute(w, &msg)
+		} // End empty fields
 
-		log.Printf("Response status: %s", resp.Status)
-
-		// templ, _ := template.ParseFiles(
-		// 	"template/add.html",
-		// 	"template/err.html",
-		// )
-
-		// templ.Execute(w, nil)
 		
 	}
 	templ, _ := template.ParseFiles(
